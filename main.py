fa2321f9013156cc0469db75125bb94a522457d7
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-18 22:03:18 +0800

    change draw board logic

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
 class TicTacToe:
     def __init__(self):
-        self.board = []
         self.player_inputs = []
+        self.tl = " "
+        self.t = " "
+        self.tr = " "
+        self.ml = " "
+        self.m = " "
+        self.mr = " "
+        self.dl = " "
+        self.d = " "
+        self.dr = " "
+        self.board = [[f" {self.tl} |", f" {self.t} |", f" {self.tr} "],
+                      [f" {self.ml} |", f" {self.m} |", f" {self.mr} "],
+                      [f" {self.dl} |", f" {self.d} |", f" {self.dr} "]]
 
     def draw_board(self):
-        for i in range(2):
-            print(2 * '   |')
-            print(2 * '   |')
-            print(2 * '   |')
-            print(3 * '----')
-        print(2 * '   |')
-        print(2 * '   |')
-        print(2 * '   |')
+        for i in range(3):
+            for col in self.board[i]:
+                print(col, end="")
+            print()
+            if i < 2:
+                print(11 * '-')
 
+                    
 game = TicTacToe()
 game.draw_board()

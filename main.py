ba522d2437a8e177531fbb1bbdc8be582d58ec6b
John Isaac Delgado <ji23delgado@gmail.com>
2021-02-19 17:19:29 +0800

    make basic ai opponent

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import random
+
+
 class TicTacToe:
     def __init__(self):
-        self.player_inputs = []
+        self.slot_obj = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
         self.input_counter = 0
         self.game_play = True
-        self.tl = " "
+        self.slot_obj[0] = " "
         self.t = " "
         self.tr = " "
         self.ml = " "
@@ -12,9 +15,9 @@ class TicTacToe:
         self.dl = " "
         self.d = " "
         self.dr = " "
-        self.board = [[f" {self.tl} |", f" {self.t} |", f" {self.tr} "],
-                      [f" {self.ml} |", f" {self.m} |", f" {self.mr} "],
-                      [f" {self.dl} |", f" {self.d} |", f" {self.dr} "]]
+        self.board = [[f" {self.slot_obj[0]} |", f" {self.slot_obj[1]} |", f" {self.slot_obj[2]} "],
+                      [f" {self.slot_obj[3]} |", f" {self.slot_obj[4]} |", f" {self.slot_obj[5]} "],
+                      [f" {self.slot_obj[6]} |", f" {self.slot_obj[7]} |", f" {self.slot_obj[8]} "]]
 
     def draw_board(self):
         for i in range(3):
@@ -38,95 +41,115 @@ class TicTacToe:
             print()
 
             if slot == 1:
-                if self.is_filled(self.tl):
+                if self.is_filled(self.slot_obj[0]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.tl = 'x'
+                    self.slot_obj[0] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 2:
-                if self.is_filled(self.t):
+                if self.is_filled(self.slot_obj[1]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.t = 'x'
+                    self.slot_obj[1] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 3:
-                if self.is_filled(self.tr):
+                if self.is_filled(self.slot_obj[2]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.tr = 'x'
+                    self.slot_obj[2] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 4:
-                if self.is_filled(self.ml):
+                if self.is_filled(self.slot_obj[3]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.ml = 'x'
+                    self.slot_obj[3] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 5:
-                if self.is_filled(self.m):
+                if self.is_filled(self.slot_obj[4]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.m = 'x'
+                    self.slot_obj[4] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 6:
-                if self.is_filled(self.mr):
+                if self.is_filled(self.slot_obj[5]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.mr = 'x'
+                    self.slot_obj[5] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 7:
-                if self.is_filled(self.dl):
+                if self.is_filled(self.slot_obj[6]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.dl = 'x'
+                    self.slot_obj[6] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 8:
-                if self.is_filled(self.d):
+                if self.is_filled(self.slot_obj[7]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.d = 'x'
+                    self.slot_obj[7] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
             if slot == 9:
-                if self.is_filled(self.dr):
+                if self.is_filled(self.slot_obj[8]):
                     print("The slot if already filled in. Please choose another slot.")
                 else:
-                    self.dr = 'x'
+                    self.slot_obj[8] = 'x'
                     self.update_board()
                     self.input_counter += 1
                     self.draw_board()
 
-            self.check_win_status()
+            self.game_play = self.check_win_status()
 
     def update_board(self):
-        self.board = [[f" {self.tl} |", f" {self.t} |", f" {self.tr} "],
-                      [f" {self.ml} |", f" {self.m} |", f" {self.mr} "],
-                      [f" {self.dl} |", f" {self.d} |", f" {self.dr} "]]
+        self.board = [[f" {self.slot_obj[0]} |", f" {self.slot_obj[1]} |", f" {self.slot_obj[2]} "],
+                      [f" {self.slot_obj[3]} |", f" {self.slot_obj[4]} |", f" {self.slot_obj[5]} "],
+                      [f" {self.slot_obj[6]} |", f" {self.slot_obj[7]} |", f" {self.slot_obj[8]} "]]
 
     def check_win_status(self):
-        if ((self.tl == self.t and self.t == self.tr and self.tl != " ") or
-            (self.ml == self.m and self.m == self.mr and self.ml != " ") or
-            (self.dl == self.d and self.d == self.dr and self.dl != " ") or
-            (self.tl == self.ml and self.ml == self.dl and self.tl != " ") or
-            (self.t == self.m and self.m == self.d and self.d != " ") or
-            (self.tr == self.mr and self.mr == self.dr and self.dr != " ") or
-            (self.tl == self.m and self.m == self.dr and self.tl != " ") or
-        (self.tr == self.m and self.m == self.dl and self.tr != " ")):
-            self.game_play = False
+        if ((self.slot_obj[0] == self.slot_obj[1] and self.slot_obj[1] == self.slot_obj[2] and self.slot_obj[0] != " ")
+           or(self.slot_obj[3] == self.slot_obj[4] and self.slot_obj[4] == self.slot_obj[5] and self.slot_obj[5] != " ")
+           or(self.slot_obj[6] == self.slot_obj[7] and self.slot_obj[7] == self.slot_obj[8] and self.slot_obj[7] != " ")
+           or(self.slot_obj[0] == self.slot_obj[3] and self.slot_obj[3] == self.slot_obj[6] and self.slot_obj[0] != " ")
+           or(self.slot_obj[1] == self.slot_obj[4] and self.slot_obj[4] == self.slot_obj[7] and self.slot_obj[7] != " ")
+           or(self.slot_obj[2] == self.slot_obj[5] and self.slot_obj[5] == self.slot_obj[8] and self.slot_obj[8] != " ")
+           or(self.slot_obj[0] == self.slot_obj[4] and self.slot_obj[4] == self.slot_obj[8] and self.slot_obj[0] != " ")
+        or(self.slot_obj[2] == self.slot_obj[4] and self.slot_obj[4] == self.slot_obj[6] and self.slot_obj[6] != " ")):
+            return False
+
+    def opponent_ai(self):
+        for i in range(9):
+            self.slot_obj[i] = "x"
+            if not self.check_win_status():
+                self.slot_obj[i] = "o"
+                self.input_counter += 1
+                break
+            elif i == 8:
+                self.random_slot_picker()
+            self.slot_obj[i] = " "
+
+    # def random_slot_picker(self):
+    #     while True:
+    #         num = random.randint(0, 8)
+    #         if self.slot_obj[num] == " ":
+    #             self.slot_obj[num] = "o"
+    #             self.input_counter += 1
+    #             break
+
 
 
 game = TicTacToe()
